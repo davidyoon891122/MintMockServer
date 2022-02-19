fe15c20631ece419db4c94d2ac764d52b57480c3
davidyoon <qqpo12@naver.com>
2022-02-19 11:24:13 +0900

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+// simple-expressjs/index.js
+const express = require('express');
+const app = express();
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send('Hello');
+});
+
+app.listen(port, () => {
+  console.log('Server is running', port);
+});
