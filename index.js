fe1a7d176a62b9c70f2ac7dd366da5cff74edb5d
davidyoon <qqpo12@naver.com>
2022-02-20 17:02:12 +0900

    add functions to make randomJSONList for myStock

--- index.js
+++ index.js
@@ -8,62 +8,9 @@ app.get("/", (req, res) => {
   res.send('Hello');
 });
 
-app.get("/test", (req, res) => {
-    res.send('Test');
-  });
-
-app.get("/res-test", (req, res) => {
-    res.status(200).send('response 200');
-});
-
-app.get("/res-test404", (req, res) => {
-    res.status(404).send('response 404');
-});
-
-app.get("/json-test", (req, res) => {
-    const resJson = {
-        "name": "yoon"
-    }
-    res.status(200).json(resJson)
-});
-
 app.get("/my-stock", (req, res) => {
-    createMyStockLists(5)
-    
-
-    const resJson = [   
-        { 
-            "stockName": "AT&T",
-            "currentPrice": 2150,
-            "sotckQuantity": 0.075269,
-            "valueChange": 156,
-            "percentChange": 7.82,
-        },
-        { 
-            "stockName": "AT&T",
-            "currentPrice": 2150,
-            "sotckQuantity": 0.075269,
-            "valueChange": 156,
-            "percentChange": 7.82,
-        },
-        { 
-            "stockName": "AT&T",
-            "currentPrice": 2150,
-            "sotckQuantity": 0.075269,
-            "valueChange": 156,
-            "percentChange": 7.82,
-        },
-        { 
-            "stockName": "AT&T",
-            "currentPrice": 2150,
-            "sotckQuantity": 0.075269,
-            "valueChange": 156,
-            "percentChange": 7.82,
-        },
-
-    ]
-    
-    res.status(200).json(resJson)
+    const myStocksJSON = createMyStockLists(5)
+    res.status(200).json(myStocksJSON)
 });
 
 app.listen(port, () => {
--- stocks.js
+++ stocks.js
@@ -5,8 +5,27 @@ import { json } from "express";
 export function createMyStockLists(count) {
     
     let list = []
+    Array.from(Array(count).keys())
+    .forEach(element => {
+        const randomJson = getRandomJSON()
+        list.push(randomJson);
+    });
+    console.log(list)
+    return list
+}
+
+function getRandomNumber(min, max, isInt) {
+    if (isInt) {
+        return Math.round(Math.random() * (max - min) + min);
+    }
+    return Math.random() * (max - min) + min;
+}
+
+function getRandomJSON() {
+
     const stocks = ["AT&T", "유아이패스", "애플", "테슬라", "서던", "슐럼버거"
     ,"소파이 테크놀로지", "루시드 그룹", "필립스 66", "파이오니어 내츄럴 리소스"]
+
 
     let jsonFormat = {
         "stockName": "AT&T",
@@ -15,25 +34,12 @@ export function createMyStockLists(count) {
         "valueChange": 156,
         "percentChange": 7.82,
     }
-    // var item = items[Math.floor(Math.random()*items.length)];
-    Array.from(Array(count).keys())
-    .forEach(element => {
-        
 
-        jsonFormat.stockName = stocks[Math.floor(Math.random() * stocks.length)]
-        jsonFormat.currentPrice = getRandomNumber(0, 30000, true);
-        jsonFormat.stockQuantity = Math.random();
-        jsonFormat.valueChange = getRandomNumber(0, 10000, true);
-        jsonFormat.percentChange = getRandomNumber(0, 30, false);
+    jsonFormat.stockName = stocks[Math.floor(Math.random() * stocks.length)]
+    jsonFormat.currentPrice = getRandomNumber(0, 30000, true);
+    jsonFormat.stockQuantity = Math.random();
+    jsonFormat.valueChange = getRandomNumber(0, 10000, true);
+    jsonFormat.percentChange = getRandomNumber(0, 30, false);
 
-        console.log(jsonFormat)
-
-    });
-}
-
-function getRandomNumber(min, max, isInt) {
-    if (isInt) {
-        return Math.round(Math.random() * (max - min) + min);
-    }
-    return Math.random() * (max - min) + min;
+    return jsonFormat
 }
