527d601dc8b8a9e6f8e2cf3cfd4a1245d8e202b5
Yoonjiwon <k112751@koreainvestment.com>
2022-05-31 14:23:57 +0900

    apply prettier

--- src/main.js
+++ src/main.js
@@ -1,34 +1,39 @@
-// simple-expressjs/index.js
+// @ts-check
 const express = require('express')
 
-const { createDividendStockList, createIncreaseStockList, createMyProfit, createMyStockLists } = require("../stocks.js")
-const app = express();
-const port = process.env.PORT;
+const {
+  createDividendStockList,
+  createIncreaseStockList,
+  createMyProfit,
+  createMyStockLists,
+} = require('../stocks.js')
+const app = express()
+const port = process.env.PORT
 
-app.get("/", (req, res) => {
-  res.send('Hello');
-});
-
-app.get("/my-stock", (req, res) => {
-    const myStocksJSON = createMyStockLists(5);
-    res.status(200).json(myStocksJSON);
-});
-
-app.get("/my-profit", (req, res) => {
-  const myProfitJSON = createMyProfit();
-  res.status(200).json(myProfitJSON);
+app.get('/', (req, res) => {
+  res.send('Hello')
 })
 
-app.get("/dividend-list", (req, res) => {
-  const myDividendsJSON = createDividendStockList(10);
-  res.status(200).json(myDividendsJSON);
+app.get('/my-stock', (req, res) => {
+  const myStocksJSON = createMyStockLists(5)
+  res.status(200).json(myStocksJSON)
 })
 
-app.get("/increase-list", (req, res) => {
-  const myIncreaseStocksJSON = createIncreaseStockList(10);
+app.get('/my-profit', (req, res) => {
+  const myProfitJSON = createMyProfit()
+  res.status(200).json(myProfitJSON)
+})
+
+app.get('/dividend-list', (req, res) => {
+  const myDividendsJSON = createDividendStockList(10)
+  res.status(200).json(myDividendsJSON)
+})
+
+app.get('/increase-list', (req, res) => {
+  const myIncreaseStocksJSON = createIncreaseStockList(10)
   res.status(200).json(myIncreaseStocksJSON)
 })
 
 app.listen(port, () => {
-  console.log('Server is running', port);
-});
+  console.log('Server is running', port)
+})
--- stocks.js
+++ stocks.js
@@ -1,118 +1,114 @@
-const { stocks, stockImageURL } = require("./stockInfo.js")
+const { stocks, stockImageURL } = require('./stockInfo.js')
 
 function createMyStockLists(count) {
-    let list = []
-    Array.from(Array(count).keys())
-    .forEach(element => {
-        const randomJSON = getRandomMyStockJSON();
-        list.push(randomJSON);
-    });
-    console.log(list);
-    return list
+  let list = []
+  Array.from(Array(count).keys()).forEach((element) => {
+    const randomJSON = getRandomMyStockJSON()
+    list.push(randomJSON)
+  })
+  console.log(list)
+  return list
 }
 
 function createMyProfit() {
-    let list = []
-    let jsonFormat = {
-        "userName": "관리자",
-        "totalAsset": getRandomNumber(1000,10000000, true),
-        "valueChange": getRandomNumber(0, 10000, true),
-        "percentChange": getRandomNumber(0, 300, false),
-        "referenceDay": "20220223"
-    }
-    
-    list.push(jsonFormat);
-    return list
+  let list = []
+  let jsonFormat = {
+    userName: '관리자',
+    totalAsset: getRandomNumber(1000, 10000000, true),
+    valueChange: getRandomNumber(0, 10000, true),
+    percentChange: getRandomNumber(0, 300, false),
+    referenceDay: '20220223',
+  }
+
+  list.push(jsonFormat)
+  return list
 }
 
 function createDividendStockList(count) {
-    let list = []
-    Array.from(Array(count).keys())
-    .forEach(element => {
-        const randomJSON = getRandomDividendStockJSON();
-        list.push(randomJSON);
-    });
-    console.log(list)
-    return list
+  let list = []
+  Array.from(Array(count).keys()).forEach((element) => {
+    const randomJSON = getRandomDividendStockJSON()
+    list.push(randomJSON)
+  })
+  console.log(list)
+  return list
 }
 
 function createIncreaseStockList(count) {
-    let list = []
-    Array.from(Array(count).keys())
-    .forEach(element => {
-        const randomJSON = getRandomIncreaseStockJSON();
-        list.push(randomJSON);
-    });
-    return list
+  let list = []
+  Array.from(Array(count).keys()).forEach((element) => {
+    const randomJSON = getRandomIncreaseStockJSON()
+    list.push(randomJSON)
+  })
+  return list
 }
 
-
 function getRandomNumber(min, max, isInt) {
-    if (isInt) {
-        return Math.round(Math.random() * (max - min) + min);
-    }
-    return Math.random() * (max - min) + min;
+  if (isInt) {
+    return Math.round(Math.random() * (max - min) + min)
+  }
+  return Math.random() * (max - min) + min
 }
 
 function getRandomMyStockJSON() {
-    let jsonFormat = {
-        "stockName": "AT&T",
-        "currentPrice": 2150,
-        "stockQuantity": 0.075269,
-        "valueChange": 156,
-        "percentChange": 7.82,
-        "imageURL": ""
-    }
+  let jsonFormat = {
+    stockName: 'AT&T',
+    currentPrice: 2150,
+    stockQuantity: 0.075269,
+    valueChange: 156,
+    percentChange: 7.82,
+    imageURL: '',
+  }
 
-    jsonFormat.stockName = stocks[Math.floor(Math.random() * stocks.length)]
-    jsonFormat.currentPrice = getRandomNumber(0, 30000, true);
-    jsonFormat.stockQuantity = Math.random();
-    jsonFormat.valueChange = getRandomNumber(0, 10000, true);
-    jsonFormat.percentChange = getRandomNumber(0, 30, false);
-    jsonFormat.imageURL = stockImageURL[jsonFormat.stockName];
+  jsonFormat.stockName = stocks[Math.floor(Math.random() * stocks.length)]
+  jsonFormat.currentPrice = getRandomNumber(0, 30000, true)
+  jsonFormat.stockQuantity = Math.random()
+  jsonFormat.valueChange = getRandomNumber(0, 10000, true)
+  jsonFormat.percentChange = getRandomNumber(0, 30, false)
+  jsonFormat.imageURL = stockImageURL[jsonFormat.stockName]
 
-    return jsonFormat
+  return jsonFormat
 }
 
 function getRandomDividendStockJSON() {
-    let jsonFormat = {
-        "stockName": "AT&T",
-        "currentPrice": 2150,
-        "percentChange": 7.82,
-        "imageURL": "",
-        "exDividendDate": "2월 28일",
-    }
+  let jsonFormat = {
+    stockName: 'AT&T',
+    currentPrice: 2150,
+    percentChange: 7.82,
+    imageURL: '',
+    exDividendDate: '2월 28일',
+  }
 
-    jsonFormat.stockName = stocks[Math.floor(Math.random() * stocks.length)]
-    jsonFormat.currentPrice = getRandomNumber(0, 30000, true);
-    jsonFormat.percentChange = getRandomNumber(0, 30, false);
-    jsonFormat.imageURL = stockImageURL[jsonFormat.stockName];
-    jsonFormat.exDividendDate = "2월 28일" 
+  jsonFormat.stockName = stocks[Math.floor(Math.random() * stocks.length)]
+  jsonFormat.currentPrice = getRandomNumber(0, 30000, true)
+  jsonFormat.percentChange = getRandomNumber(0, 30, false)
+  jsonFormat.imageURL = stockImageURL[jsonFormat.stockName]
+  jsonFormat.exDividendDate = '2월 28일'
 
-    return jsonFormat
+  return jsonFormat
 }
 
 function getRandomIncreaseStockJSON() {
-    let jsonFormat = {
-        "stockName": "AT&T",
-        "stockCode": "AAA",
-        "percentChange:": 4.34,
-        "currentPrice": 2150,
-        "imageURL": "",
-    }
+  let jsonFormat = {
+    stockName: 'AT&T',
+    stockCode: 'AAA',
+    'percentChange:': 4.34,
+    currentPrice: 2150,
+    imageURL: '',
+  }
 
-    jsonFormat.stockName = stocks[Math.floor(Math.random() * stocks.length)]
-    jsonFormat.stockCode = "AAA"
-    jsonFormat.percentChange = getRandomNumber(0, 30, false);
-    jsonFormat.currentPrice = getRandomNumber(0, 30000, true);
-    jsonFormat.imageURL = stockImageURL[jsonFormat.stockName];
+  jsonFormat.stockName = stocks[Math.floor(Math.random() * stocks.length)]
+  jsonFormat.stockCode = 'AAA'
+  jsonFormat.percentChange = getRandomNumber(0, 30, false)
+  jsonFormat.currentPrice = getRandomNumber(0, 30000, true)
+  jsonFormat.imageURL = stockImageURL[jsonFormat.stockName]
 
-    return jsonFormat
+  return jsonFormat
 }
 
 module.exports = {
-    createMyStockLists,
-    createMyProfit,
-    createDividendStockList,
-    createIncreaseStockList
+  createMyStockLists,
+  createMyProfit,
+  createDividendStockList,
+  createIncreaseStockList,
 }
