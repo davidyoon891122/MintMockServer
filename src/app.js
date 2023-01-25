126d572bf0c3074f617b693cb71c042964536046
Davidyoon <davidyoon@koreainvestment.com>
2023-01-25 13:15:49 +0900

    Add socket io functions
    * Applied a cors
    * Added a cors
    * Added functions for the socket io

--- src/app.js
+++ src/app.js
@@ -1,6 +1,15 @@
 // @ts-check
 const express = require('express')
 const app = express()
+const cors = require("cors")
+const http = require("http")
+app.use(cors())
+
+const server = http.createServer(app)
+const { Server } = require("socket.io")
+const io = new Server(server)
+
+const sise = io.of("/sise")
 
 const {
   createDividendStockList,
@@ -8,6 +17,7 @@ const {
   createMyProfit,
   createMyStockLists,
 } = require('./stocks.js')
+const { Socket } = require('engine.io')
 
 app.get('/', (req, res) => {
   res.send('Hello')
@@ -33,4 +43,34 @@ app.get('/increase-list', (req, res) => {
   res.status(200).json(myIncreaseStocksJSON)
 })
 
-module.exports = app
+io.on('connection', (socket) => {
+  console.log("a user connected");
+  
+  socket.on('disconnect', (reason) => {
+    console.log("disconnected")
+  })
+
+  socket.on('message', (message) => {
+    io.emit("message", `messsage from the server: ${message}`)
+  })
+
+  socket.on('join', (roomName) => {
+    console.log(`request join to : ${roomName}`)
+    socket.join(roomName)
+  })
+})
+
+sise.on('connection', (socket) => {
+  console.log('Client has connected to sise namespace')
+
+  socket.on('disconnect', () => {
+    console.log('The client has disconnected')
+  })
+
+  socket.on('service', (serviceName) => {
+    console.log(`client request ${serviceName}..`)
+    socket.emit("service", "This message is from sise server")
+  })
+})
+
+module.exports = server
